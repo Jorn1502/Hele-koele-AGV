706f96d13c316a63187402838f112c793122cc6c
A. de Graaf <arik.degraaf@gmail.com>
2023-06-09 19:19:04 +0200

    IR ADC
    Begonnen met ADC voor IR sensor

--- irSensor/main.c
+++ irSensor/main.c
@@ -5,6 +5,10 @@
 
 #define sensor1 PK1
 #define LED1 PB7
+#define irSensor PK0
+
+//PK0 ( ADC8/PCINT16 ) Analog pin 8 MUX5:0 100000
+
 
 int sensorVal = 0;
 
@@ -13,6 +17,12 @@ void init()
     DDRK &= ~(1<<sensor1);
     DDRB |= (1<<LED1);
     PORTB |= (1<<LED1);
+
+    ADMUX = (0 << REFS1) | (1 << REFS0);
+    ADCSRB |= (1<<MUX5);
+    ADCSRA = (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+
 }
 
 void getBoom(void)
